fd00500ba5c15dc07a8acecef14fc77eab65ee79
alazar-w <ej.dalvic@gmail.com>
2021-05-27 09:25:07 +0300

    pupular sction image altered,first app phase review done!

--- lib/screens/HomePage/widgets/card_item.dart
+++ lib/screens/HomePage/widgets/card_item.dart
@@ -13,7 +13,7 @@ class CardItem extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4.0),
       child: ListTile(
         dense: false,
-        leading:  ClipRRect(borderRadius: BorderRadius.circular(10),child: Image.asset("images/testImage2.jpg", width: 80, height: 100,fit: BoxFit.cover,)),
+        leading:  ClipRRect(borderRadius: BorderRadius.circular(10),child: Image.asset("images/popularSection.jpg", width: 80, height: 100,fit: BoxFit.cover,)),
         title: Text("European Tour",style: kHeadlineTextStyle,),
         subtitle: Text('14 april - 25 april 2021'),
       ),
